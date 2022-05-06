392ba82fb921e120e72bd6e053757a644de334c9
Mattia-Zanini <mattzanini2@gmail.com>
2022-05-06 08:33:22 +0200

    Ok now it's completed

--- Program.cs
+++ Program.cs
@@ -8,41 +8,31 @@ namespace CreditCardValidation
         static void Main(string[] args)
         {
             bool isValid = false;
-            string[] PAN = GeneratePAN();
+            int[] PAN = new int[16];
+            string strPAN = "";
             do
             {
                 int tot = 0;
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < 16; i++)
                 {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        string ch = PAN[i][j].ToString();
-                        //Console.WriteLine(ch);
-                        if (j % 2 == 0)
-                            tot += 2 * Convert.ToInt32(ch);
-                        else
-                            tot += Convert.ToInt32(ch);
-                    }
+                    PAN[i] = rand.Next(0, 10);
+                    strPAN += PAN[i].ToString();
+                    //Console.WriteLine(PAN[i]);
+                    if (i % 2 == 0)
+                        tot += 2 * PAN[i];
+                    else
+                        tot += PAN[i];
                 }
                 if (tot % 10 == 0)
                     isValid = true;
                 else
                 {
                     Console.WriteLine("Invalid PAN\n");
-                    PAN = GeneratePAN();
+                    PAN = new int[16];
+                    strPAN = "";
                 }
             } while (!isValid);
-            Console.WriteLine($"Valid PAN:\n{PAN[0]} {PAN[1]} {PAN[2]} {PAN[3]}");
-        }
-        static string[] GeneratePAN()
-        {
-            string[] PAN = new string[4];
-            for (int i = 0; i < 4; i++)
-            {
-                PAN[i] = rand.Next(1111, 10000).ToString();
-                //Console.WriteLine("Generated PAN: " + PAN[i]);
-            }
-            return PAN;
+            Console.WriteLine($"Valid PAN:\n{strPAN}");
         }
     }
 }
